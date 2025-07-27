3259ea2dec65223accccbb20f2a8c283b0ddaa4a
Farhan Mubeen <118782329+farhan-mubeen@users.noreply.github.com>
2025-07-27 16:16:21 +0530

    Add files via upload

---
+++ assets/js/carousel.js
@@ -0,0 +1,101 @@
+// Home Page 1 
+
+$(document).ready(function () {
+    var owl = $('.testimonial-con .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 2
+            }
+        }
+    })
+})
+
+
+$(document).ready(function () {
+    var owl = $('.article-con .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
+})
+
+// Home Page 2 
+
+$(document).ready(function () {
+    var owl = $('.choose2-con .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
+    })
+})
+
+// Home Page 3 
+
+$(document).ready(function () {
+    var owl = $('.testimonial3-con .owl-carousel');
+    owl.owlCarousel({
+        margin: 30,
+        nav: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    })
+})
